9e71fc83a4bfdea656e4eaefb152df48c470fcca
Haigsov <haigsouv@my.yorku.ca>
2025-05-21 23:52:28 -0400

    Update Mob.cs

--- src/mob/Mob.cs
+++ src/mob/Mob.cs
@@ -9,12 +9,13 @@ public partial class Mob : RigidBody2D
 	{
 		GravityScale = 0;
 		SetCollisionMaskValue(1, false);
-		AnimatedSprite2D animatedSprite2D  = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		AnimatedSprite2D animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+		animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void OnVisibleOnScreenNotifier2DScreenExited()
 	{
+		QueueFree();
 	}
 }
