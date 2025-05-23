a31b455b4ab76fdb3f7c72600dbe7a9d0dc99837
Haigsov <haigsouv@my.yorku.ca>
2025-05-23 18:48:52 -0400

    Trying to understand how C# signals work

--- src/main/Main.cs
+++ src/main/Main.cs
@@ -6,15 +6,17 @@ public partial class Main : Node2D
 	// references mob scene
 	[Export]
 	public PackedScene MobScene { get; set; }
+	private CharacterBody2D Player;
 	// score of the game
 	private int _score;
 
 	public override void _Ready()
 	{
-		 //EmitSignal()
+		Player = GetNode<CharacterBody2D>("Player");
+		//Player.Hit += GameOverSignal;
 	}
 
-	public void GameOver()
+	public void GameOverSignal()
 	{
 		GetNode<Timer>("MobTimer").Stop();
 		GetNode<Timer>("ScoreTimer").Stop();
