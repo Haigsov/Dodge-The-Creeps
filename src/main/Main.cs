1e677b37777a48389b962fd47fb58288bc4b5366
Haigsov <haigsouv@my.yorku.ca>
2025-05-24 22:06:06 -0400

    Fixed signal issue I was having

--- src/main/Main.cs
+++ src/main/Main.cs
@@ -6,14 +6,14 @@ public partial class Main : Node2D
 	// references mob scene
 	[Export]
 	public PackedScene MobScene { get; set; }
-	private CharacterBody2D Player;
+	public Player Player;
 	// score of the game
 	private int _score;
 
 	public override void _Ready()
 	{
-		Player = GetNode<CharacterBody2D>("Player");
-		//Player.Hit += GameOverSignal;
+		Player = GetNode<Player>("Player");
+		Player.Hit += GameOverSignal;
 	}
 
 	public void GameOverSignal()
