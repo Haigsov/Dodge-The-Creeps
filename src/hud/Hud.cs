de6670e67f564df479d5954eb779a4fc8bf65b41
Haigsov <haigsouv@my.yorku.ca>
2025-05-31 23:58:35 -0400

    Created custom signal and new function

--- src/hud/Hud.cs
+++ src/hud/Hud.cs
@@ -3,4 +3,15 @@ using System;
 
 public partial class Hud : CanvasLayer
 {
+    [Signal]
+    public delegate void StartGameEventHandler();
+
+    public void ShowMessage(string text)
+    {
+        Label message = GetNode<Label>("Message");
+        message.Text = text;
+        message.Show();
+
+        GetNode<Timer>("MessageTimer").Start();
+    }
 }
