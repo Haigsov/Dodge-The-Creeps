47ca8fd163076ebe30b6dc91216a92c4b45ae008
Haigsov <haigsouv@my.yorku.ca>
2025-06-02 22:33:54 -0400

    Added few more signals and functionality to HUD

--- src/hud/Hud.cs
+++ src/hud/Hud.cs
@@ -6,7 +6,13 @@ public partial class Hud : CanvasLayer
     [Signal]
     public delegate void StartGameEventHandler();
 
-    // Shows text.
+    public override void _Ready()
+    {
+        GetNode<Button>("StartButton").Pressed += OnStartButtonPressedSignal;
+        GetNode<Timer>("MessageTimer").Timeout += OnMessageTimerTimeoutSignal;
+    }
+
+    // Function that allows you to change the text in Message label
     public void ShowMessage(string text)
     {
         Label message = GetNode<Label>("Message");
@@ -16,6 +22,7 @@ public partial class Hud : CanvasLayer
         GetNode<Timer>("MessageTimer").Start();
     }
 
+    // Starts Game Over! sequence and then lets you replay the game again.
     async public void ShowGameOver()
     {
         ShowMessage("Game Over!");
@@ -25,7 +32,27 @@ public partial class Hud : CanvasLayer
 
         ShowMessage("Dodge the Creeps!");
 
+        // Created a timer within code instead of creating a new timer as a node.
         await ToSignal(GetTree().CreateTimer(1.0), SceneTreeTimer.SignalName.Timeout);
         GetNode<Button>("StartButton").Show();
+    }
+
+    // Takes ScoreLabel and updates it.
+    public void UpdateScore(int score)
+    {
+        GetNode<Label>("ScoreLabel").Text = score.ToString();
+    }
+
+    // Hides StartButton and starts game when StartButton is pressed.
+    private void OnStartButtonPressedSignal()
+    {
+        GetNode<Button>("StartButton").Hide();
+        EmitSignal(SignalName.StartGame);
+    }
+
+    // Hides the Message when timer finishes countdown through timeout signal.
+    private void OnMessageTimerTimeoutSignal()
+    {
+        GetNode<Label>("Message").Hide();
     }
 }
